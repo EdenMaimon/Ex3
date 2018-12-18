0b8c9d4a3858276e3ac834581c226df87d2bc23d
EdenMaimon <eden.maimon@live.biu.ac.il>
2018-12-18 22:11:44 +0200

    Data Server building

---
+++ DataServerReader.h
@@ -0,0 +1,17 @@
+//
+// Created by eden on 12/18/18.
+//
+
+#ifndef EX3_DATASERVERREADER_H
+#define EX3_DATASERVERREADER_H
+
+class DataServerReader{
+
+public:
+    void openserver(int port int hz);
+private:
+
+};
+
+
+#endif //EX3_DATASERVERREADER_H
